03edf9aae0f246400a3831eee4a3dbdb44b80912
sinedied <noda@free.fr>
2024-01-11 17:34:55 +0100

    test: try fix again

--- index.test.js
+++ index.test.js
@@ -1,4 +1,3 @@
-const { EOL } = require("os");
 const path = require('path');
 const execSync = require('child_process').execSync;
 
@@ -8,7 +7,7 @@ function testCli(args, expectedOutput, cwd = 'test') {
     args = args.split(' ');
   
     const result = execSync(`node ${fuzzrun} ${args}`, { encoding: 'utf-8', cwd });
-    const lastLine = result.split(EOL).filter(Boolean).pop();
+    const lastLine = result.split(/\\r?\\n/).filter(Boolean).pop();
     expect(lastLine).toBe(expectedOutput);
   });
 }
