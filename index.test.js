655e7c2a24c28eacd68301c0863a5fa3ad568898
sinedied <noda@free.fr>
2024-01-11 17:31:09 +0100

    test: fix eol on windows

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { EOL } = require("os");
 const path = require('path');
 const execSync = require('child_process').execSync;
 
@@ -7,7 +8,7 @@ function testCli(args, expectedOutput, cwd = 'test') {
     args = args.split(' ');
   
     const result = execSync(`node ${fuzzrun} ${args}`, { encoding: 'utf-8', cwd });
-    const lastLine = result.split('\n').filter(Boolean).pop();
+    const lastLine = result.split(EOL).filter(Boolean).pop();
     expect(lastLine).toBe(expectedOutput);
   });
 }
